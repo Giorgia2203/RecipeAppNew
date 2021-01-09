9f7e7a80b42fcad0066b98277e33edbc94ec506c
giorgia220523 <cocisgiorgia@gmail.com>
2021-01-09 19:39:12 +0200

    Chat

--- RecipeMVC/wwwroot/js/chat.js
+++ RecipeMVC/wwwroot/js/chat.js
@@ -5,11 +5,31 @@ document.getElementById("sendButton").disabled = true;
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g,
         "&gt;");
-    var encodedMsg = user + " : " + msg;
     var li = document.createElement("li");
-    li.textContent = encodedMsg;
+
+    var divContainer = document.createElement("div");
+    divContainer.className = "card border-secondary mb-3";
+    divContainer.style.maxWidth = "20rem";
+
+    var divHeader = document.createElement("div");
+    divHeader.className = "card-header text-white bg-secondary";
+    divHeader.style.fontWeight = "bold";
+    divHeader.innerHTML = user;
+
+    var divBody = document.createElement("div");
+    divBody.className = "card-body";
+
+    var pContent = document.createElement("p");
+    pContent.innerHTML = msg;
+
+    divBody.appendChild(pContent);
+    divContainer.appendChild(divHeader);
+    divContainer.appendChild(divBody);
+
+    console.log(divContainer)
+    li.appendChild(divContainer);
+    console.log(li)
     document.getElementById("messagesList").appendChild(li);
- 
 });
 
 connection.start().then(function () {
